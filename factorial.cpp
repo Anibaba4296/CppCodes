14d58380586d7b91974c4eeb1bf0d43561b32764
Aniket Kumar <103889271+Anibaba4296@users.noreply.github.com>
2022-06-10 12:28:38 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int num){
+    int factorial=1;
+    for(int i=2;i<=num;i++){
+        factorial*=i;
+    }
+    return factorial;
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=fact(n);
+    cout<<ans;
+    return 0;
+}
